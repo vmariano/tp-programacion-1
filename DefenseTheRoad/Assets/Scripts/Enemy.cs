a07c465acf407ef2a7f2978b045b290d642d8e48
Mariano <m.vicent@gmail.com>
2016-09-27 20:02:37 -0300

    Add basic Enemy Move.

---
+++ DefenseTheRoad/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Rigidbody2D Body;
+    public float Speed = 1f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        Vector3 direction = new Vector3();
+        direction.x -= 1f ;
+        direction.y -= 1f;
+        Body.velocity = direction * Speed;
+	    Debug.Log(direction);
+        if (Body.position.x < -6 || Body.position.y < -5  )
+	    {
+	        Destroy(this);
+	    }
+    }
+
+    Vector3 GetPosition()
+    {
+        return Body.velocity;
+    }
+    
+
+}
---
+++ DefenseTheRoad/Assets/Scripts/Tower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour
+{
+
+    public Rigidbody2D sprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    this.Move();
+	}
+
+    void Move() {
+        
+    }
+}
