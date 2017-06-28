03aa8437092eda74a38dbf75512756846704c194
Mariano <m.vicent@gmail.com>
2017-06-28 03:38:36 -0300

    Script de botones de menu principal

---
+++ DefenseTheRoad/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+   public string TargetLoad;
+   
+    private void OnMouseOver()
+    {
+       if (Input.GetMouseButtonDown(0))
+       {
+          if (TargetLoad.Equals("salir"))
+          {
+             Application.Quit();
+          }
+          else
+          {
+             SceneManager.LoadScene(TargetLoad);
+          }
+       }
+    }
+}
