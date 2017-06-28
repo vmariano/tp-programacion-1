3c4d387ef29e41cdfc9ca795db4241705002e053
Mariano <m.vicent@gmail.com>
2017-06-28 04:07:50 -0300

    Agrego menu funcional
    con grisado de botones.

--- DefenseTheRoad/Assets/Scripts/MenuButton.cs
+++ DefenseTheRoad/Assets/Scripts/MenuButton.cs
@@ -3,20 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
+   //Target de donde va cargar la escena
    public string TargetLoad;
-   
-    private void OnMouseOver()
-    {
-       if (Input.GetMouseButtonDown(0))
-       {
-          if (TargetLoad.Equals("salir"))
-          {
-             Application.Quit();
-          }
-          else
-          {
-             SceneManager.LoadScene(TargetLoad);
-          }
-       }
-    }
+
+   public GameObject CurrentButton;
+
+   private SpriteRenderer _sprite;
+
+   private void Start()
+   {
+      this._sprite = CurrentButton.GetComponent<SpriteRenderer>();
+   }
+
+   private void OnMouseOver()
+   {
+      this._sprite.color = Color.gray;
+      if (Input.GetMouseButtonDown(0))
+      {
+         this.LoadScene();
+      }
+   }
+
+   private void LoadScene()
+   {
+      if (TargetLoad.Equals("salir"))
+      {
+         Application.Quit();
+      }
+      else
+      {
+         SceneManager.LoadScene(TargetLoad);
+      }
+   }
+
+   private void OnMouseExit()
+   {
+      this._sprite.color = Color.white;
+   }
 }
