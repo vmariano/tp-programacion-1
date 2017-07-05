70bc00004877102023ee5044400248a98da7fe77
Mariano <m.vicent@gmail.com>
2017-07-05 02:41:26 -0300

    Aniado sonidos.
    Que se yo tiene un delay, no es lo mejor, pero al menos lo puedo
    justificar en lugar del yield, que no entendi bien como se usa.

--- DefenseTheRoad/Assets/Scripts/MenuButton.cs
+++ DefenseTheRoad/Assets/Scripts/MenuButton.cs
@@ -1,18 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
    //Target de donde va cargar la escena
    public string TargetLoad;
-
    public GameObject CurrentButton;
+   public AudioClip SoundsFx;
+   public AudioSource SoundSource;
 
    private SpriteRenderer _sprite;
 
    private void Start()
    {
       this._sprite = CurrentButton.GetComponent<SpriteRenderer>();
+      SoundSource = this.GetComponent<AudioSource>();
    }
 
    private void OnMouseOver()
@@ -20,12 +24,19 @@ public class MenuButton : MonoBehaviour
       this._sprite.color = Color.gray;
       if (Input.GetMouseButtonDown(0))
       {
+         this.PlaySound();
          this.LoadScene();
       }
    }
 
    private void LoadScene()
    {
+      if (SoundSource.isPlaying)
+      {
+         InvokeRepeating("LoadScene", 0, 1); 
+         return;
+      }
+      
       if (TargetLoad.Equals("salir"))
       {
          Application.Quit();
@@ -35,9 +46,15 @@ public class MenuButton : MonoBehaviour
          SceneManager.LoadScene(TargetLoad);
       }
    }
-
+  
    private void OnMouseExit()
    {
       this._sprite.color = Color.white;
+   }
+
+   private void PlaySound()
+   {
+      if (SoundSource.isPlaying) return;
+      SoundSource.PlayOneShot(SoundsFx);
    }
 }
