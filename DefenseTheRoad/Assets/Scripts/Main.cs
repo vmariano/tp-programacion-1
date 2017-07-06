afc01c5f5834f66ce11cd3543d21e82d114f4378
Mariano <m.vicent@gmail.com>
2017-07-06 03:10:42 -0300

    Nuevos sonidos.
    - Agregado de torre.
    - Agregado de nope sound.
    - Agregado de cancion en el main menu.

--- DefenseTheRoad/Assets/Scripts/Main.cs
+++ DefenseTheRoad/Assets/Scripts/Main.cs
@@ -3,11 +3,6 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
